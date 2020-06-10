d6a3053d2b4d5a06162d3bb6e205fba46ea7c971
tanviredu <ornobtanvir.git@gmail.com>
2020-06-10 19:59:15 +0600

    added

--- My_Blog_Project/My_Blog_Project/urls.py
+++ My_Blog_Project/My_Blog_Project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns,static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/',include('App_login.urls')),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('',views.index,name='index')
 
 ]
+
+urlpatterns+= staticfiles_urlpatterns()
+urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
